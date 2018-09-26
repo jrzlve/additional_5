5573a9abfde220417b384d0c98a3dc23fede24b2
jrzlve <jrzlve@gmail.com>
2018-09-26 17:28:00 +0300

    v.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let something = str;
+  for(let i = 0, len = str.length; i < len; i++){
+    something = something.replace("()", "");
+    something = something.replace("{}", "");
+    something = something.replace("[]", "");
+    something = something.replace("||", "");
+    something = something.replace("12", "");
+    something = something.replace("34", "");
+    something = something.replace("56", "");
+    something = something.replace("77", "");
+    something = something.replace("88", "");
+  }
+  if(something.length != 0) return false;
+  else return true;
 }
